4a8caf0d9aa9d824ec72f8b48e05f141a0ac2e1a
Nikita Yazikov <cour1337@gmail.com>
2020-07-22 03:47:15 +0300

    Implement echo server

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,22 @@
+use std::net::UdpSocket;
+
+const BUF_SIZE: usize = 4096;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = UdpSocket::bind("0.0.0.0:6543").unwrap();
+    let mut buf: Vec<u8> = vec![0; BUF_SIZE];
+
+    println!("Server started listening!");
+    let first_address = listener.peek_from(&mut buf).unwrap().1;
+    println!(
+        "Connection established. Client has IP:PORT {}:{}",
+        first_address.ip(),
+        first_address.port()
+    );
+
+    loop {
+        listener.recv_from(&mut buf).unwrap();
+        print!("{}", String::from_utf8_lossy(&buf));
+        listener.send_to(&buf, first_address).unwrap();
+    }
 }
