303308a83bb90780f340dcf6fef083ad66cec41c
unishift <unishifft@gmail.com>
2020-08-01 02:42:10 +0300

    Add proper cli argument parsing for client

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,70 +1,52 @@
-use std::env::args;
 use std::io::{stdin, BufRead};
 use std::net::UdpSocket;
-use std::thread;
 use std::str;
+use std::thread;
+
+use clap::{App, Arg};
 
 const BUF_SIZE: usize = 4096;
 
-#[derive(Default, Debug)]
 struct Params {
     src_address: String,
     dst_address: String,
-    mode: String,
+    protocol: String, // TODO: Replace with enum
 }
 
-fn parse_args() -> std::io::Result<Params> {
-    let args: Vec<String> = args().collect();
+fn parse_args() -> Params {
+    let matches = App::new("Hole punching: client")
+        .arg(
+            Arg::with_name("src_address")
+                .short('s')
+                .long("src-address")
+                .about("Address to bind client on (X.X.X.X:port)")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("dst_address")
+                .short('d')
+                .long("dst-address")
+                .about("Server address (X.X.X.X:port)")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("protocol")
+                .short('p')
+                .long("protocol")
+                .about("Which protocol to use")
+                .possible_values(&["udp"])
+                .default_value("udp")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let mut params: Params = Default::default();
-
-    let mut state = "";
-    for arg in args.iter().skip(1) {
-        match state {
-            // Search for a state
-            "" => {
-                match arg.as_str() {
-                    "-s" => state = "-s",
-                    "-d" => state = "-d",
-                    "-m" => state = "-m",
-                    _ => {
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            format!("Argument {arg} is not supported", arg = arg),
-                        ))
-                    }
-                };
-            }
-            "-s" => {
-                params.src_address = arg.clone();
-                state = "";
-            },
-            "-d" => {
-                params.dst_address = arg.clone();
-                state = "";
-            },
-            "-m" => {
-                params.mode = arg.clone()
-            }
-            _ => (),
-        };
-    }
-
-    if params.src_address.is_empty() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Source address not specified!",
-        ));
-    }
-
-    if params.dst_address.is_empty() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            format!("Destination address not specified! {:?}", params),
-        ));
-    }
-
-    Ok(params)
+    return Params {
+        src_address: matches.value_of("src_address").unwrap().to_string(),
+        dst_address: matches.value_of("dst_address").unwrap().to_string(),
+        protocol: matches.value_of("protocol").unwrap().to_string(),
+    };
 }
 
 fn spawn_recv_loop(socket: UdpSocket) {
@@ -77,10 +59,10 @@ fn spawn_recv_loop(socket: UdpSocket) {
                     let word = str::from_utf8(&buf);
                     match word {
                         Ok(text) => print!("{}", text),
-                        Err(e) => println!("Can't convert buffer to utf8 string. {:?}", e)
+                        Err(e) => println!("Can't convert buffer to utf8 string. {:?}", e),
                     };
-                },
-                Err(e) => println!("Can't receive message from the server. {:?}", e)
+                }
+                Err(e) => println!("Can't receive message from the server. {:?}", e),
             }
         }
     });
@@ -90,18 +72,18 @@ fn connect_handler(socket: &UdpSocket, address: &str) {
     let connect_res = socket.connect(address);
     match connect_res {
         Ok(_) => println!("Connected to {}", address),
-        Err(e) => panic!(
-            "Couldn't connect to {dst}.\n{err}",
-            dst = address,
-            err = e
-        ),
+        Err(e) => panic!("Couldn't connect to {dst}.\n{err}", dst = address, err = e),
     }
 
-    socket.send(format!("Connected to {}", address).as_bytes()).expect("Couldn't send message" );
+    socket
+        .send(format!("Connected to {}", address).as_bytes())
+        .expect("Couldn't send message");
 }
 
 fn main() -> std::io::Result<()> {
-    let params = parse_args()?;
+    let params = parse_args();
+
+    println!("Using protocol {}", params.protocol.to_uppercase());
 
     let socket = UdpSocket::bind(params.src_address.as_str());
     let socket = match socket {
@@ -127,17 +109,21 @@ fn main() -> std::io::Result<()> {
     let client_address = match res {
         Ok(_) => match str::from_utf8(&buf) {
             Ok(text) => text.trim_matches(char::from(0)),
-            Err(_) => panic!("Error decoding client address")
+            Err(_) => panic!("Error decoding client address"),
         },
-        Err(_) => panic!("Error obtaining client address")
+        Err(_) => panic!("Error obtaining client address"),
     };
-    println!("Got client address {} of len {}", client_address, client_address.len());
+    println!(
+        "Got client address {} of len {}",
+        client_address,
+        client_address.len()
+    );
 
     connect_handler(&socket, client_address);
 
     spawn_recv_loop(socket.try_clone()?);
 
-    for line in stdin(). lock(). lines() {
+    for line in stdin().lock().lines() {
         let msg = line.unwrap().clone() + "\n";
         socket.send(msg.as_bytes())?;
     }
