14abacbbec274e181d6a8f10c3c2d97ce003140b
Nikita Yazikov <cour1337@gmail.com>
2020-08-01 01:20:17 +0300

    Add client ip-port exchange

--- src/server/main.rs
+++ src/server/main.rs
@@ -7,9 +7,8 @@ fn get_clients_info(listener: &UdpSocket) -> (SocketAddr, SocketAddr) {
     let first_address = listener.recv_from(&mut tmp_buf).unwrap().1;
     #[cfg(debug_assertions)]
     println!(
-        "First client connection established. ip:port {}:{}",
-        first_address.ip(),
-        first_address.port()
+        "First client connection established. {}",
+        first_address.to_string(),
     );
 
     let second_address = loop {
@@ -21,9 +20,8 @@ fn get_clients_info(listener: &UdpSocket) -> (SocketAddr, SocketAddr) {
 
     #[cfg(debug_assertions)]
     println!(
-        "Second client connection established. ip:port {}:{}",
-        second_address.ip(),
-        second_address.port()
+        "Second client connection established. {}",
+        second_address.to_string(),
     );
 
     (first_address, second_address)
@@ -34,24 +32,20 @@ fn main() {
 
     println!("Server started listening!");
 
-    let (first_client, second_client) = get_clients_info(&listener);
-
     loop {
-        let mut buf: Vec<u8> = vec![0; BUF_SIZE];
-        let recv_addr = listener.recv_from(&mut buf).unwrap().1;
+        let (first_client, second_client) = get_clients_info(&listener);
 
-        #[cfg(debug_assertions)]
-        print!("Data: {}", String::from_utf8_lossy(&buf));
-
-        let sent_data_size = if recv_addr == first_client {
-            listener.send_to(&buf, second_client).unwrap()
-        } else if recv_addr == second_client {
-            listener.send_to(&buf, first_client).unwrap()
-        }
-        else {
-            0
-        };
-        #[cfg(debug_assertions)]
-        println!("Bytes of sent data: {}", sent_data_size);
+        listener
+            .send_to(first_client.to_string().as_bytes(), second_client)
+            .unwrap_or_else(|_| {
+                println!("Error to send first client {}", second_client.to_string());
+                0
+            });
+        listener
+            .send_to(second_client.to_string().as_bytes(), first_client)
+            .unwrap_or_else(|_| {
+                println!("Error to send second client {}", second_client.to_string());
+                0
+            });
     }
 }
