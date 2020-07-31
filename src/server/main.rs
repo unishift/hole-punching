d0ccf639722d11c4cd5107b719974495aaf130b1
Yazikov Nikita <30800024+Selecao22@users.noreply.github.com>
2020-08-01 01:59:17 +0300

    Implement mvp server (#4)
    * Implement echo server
    
    * implement endpoint connection and sending data to each other
    
    * add debug sent bytes count display & change buffer size
    
    * Add client ip-port exchange

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,51 @@
+use std::net::{SocketAddr, UdpSocket};
+
+const BUF_SIZE: usize = 1024;
+
+fn get_clients_info(listener: &UdpSocket) -> (SocketAddr, SocketAddr) {
+    let mut tmp_buf: Vec<u8> = Vec::with_capacity(BUF_SIZE);
+    let first_address = listener.recv_from(&mut tmp_buf).unwrap().1;
+    #[cfg(debug_assertions)]
+    println!(
+        "First client connection established. {}",
+        first_address.to_string(),
+    );
+
+    let second_address = loop {
+        let addr = listener.recv_from(&mut tmp_buf).unwrap().1;
+        if addr != first_address {
+            break addr;
+        }
+    };
+
+    #[cfg(debug_assertions)]
+    println!(
+        "Second client connection established. {}",
+        second_address.to_string(),
+    );
+
+    (first_address, second_address)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = UdpSocket::bind("0.0.0.0:6543").unwrap();
+
+    println!("Server started listening!");
+
+    loop {
+        let (first_client, second_client) = get_clients_info(&listener);
+
+        listener
+            .send_to(first_client.to_string().as_bytes(), second_client)
+            .unwrap_or_else(|_| {
+                println!("Error to send first client {}", second_client.to_string());
+                0
+            });
+        listener
+            .send_to(second_client.to_string().as_bytes(), first_client)
+            .unwrap_or_else(|_| {
+                println!("Error to send second client {}", second_client.to_string());
+                0
+            });
+    }
 }
