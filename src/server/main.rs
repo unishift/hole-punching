9e952e540aaf5d6babc014af931f6267394e65d1
Nikita Yazikov <cour1337@gmail.com>
2020-08-01 00:43:58 +0300

    add debug sent bytes count display & change buffer size

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,10 +1,10 @@
 use std::net::{SocketAddr, UdpSocket};
 
-const BUF_SIZE: usize = 4096;
+const BUF_SIZE: usize = 1024;
 
 fn get_clients_info(listener: &UdpSocket) -> (SocketAddr, SocketAddr) {
-    let mut tmp_buff: Vec<u8> = Vec::with_capacity(BUF_SIZE);
-    let first_address = listener.recv_from(&mut tmp_buff).unwrap().1;
+    let mut tmp_buf: Vec<u8> = Vec::with_capacity(BUF_SIZE);
+    let first_address = listener.recv_from(&mut tmp_buf).unwrap().1;
     #[cfg(debug_assertions)]
     println!(
         "First client connection established. ip:port {}:{}",
@@ -13,7 +13,7 @@ fn get_clients_info(listener: &UdpSocket) -> (SocketAddr, SocketAddr) {
     );
 
     let second_address = loop {
-        let addr = listener.recv_from(&mut tmp_buff).unwrap().1;
+        let addr = listener.recv_from(&mut tmp_buf).unwrap().1;
         if addr != first_address {
             break addr;
         }
@@ -31,21 +31,27 @@ fn get_clients_info(listener: &UdpSocket) -> (SocketAddr, SocketAddr) {
 
 fn main() {
     let listener = UdpSocket::bind("0.0.0.0:6543").unwrap();
-    let mut buf: Vec<u8> = vec![0; BUF_SIZE];
 
     println!("Server started listening!");
 
     let (first_client, second_client) = get_clients_info(&listener);
 
     loop {
+        let mut buf: Vec<u8> = vec![0; BUF_SIZE];
         let recv_addr = listener.recv_from(&mut buf).unwrap().1;
+
         #[cfg(debug_assertions)]
         print!("Data: {}", String::from_utf8_lossy(&buf));
-        if recv_addr == first_client {
-            listener.send_to(&buf, second_client).unwrap();
+
+        let sent_data_size = if recv_addr == first_client {
+            listener.send_to(&buf, second_client).unwrap()
         } else if recv_addr == second_client {
-            listener.send_to(&buf, first_client).unwrap();
+            listener.send_to(&buf, first_client).unwrap()
         }
-        buf = vec![0; BUF_SIZE];
+        else {
+            0
+        };
+        #[cfg(debug_assertions)]
+        println!("Bytes of sent data: {}", sent_data_size);
     }
 }
